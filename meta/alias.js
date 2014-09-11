30002689c7e6e96245752a70604f8413b624ba96
yiminghe <yiminghe@gmail.com>
2014-09-11 22:15:12 +0800

    update alias

--- meta/alias.js
+++ meta/alias.js
@@ -1,10 +1,18 @@
-modulex.use(['modulex-ua', 'modulex-feature'], function (UA, Feature) {
-    modulex.config('alias', {
-        'modulex-dom': 'dom',
-        'dom/selector': Feature.isQuerySelectorSupported() ? '' : 'query-selector',
-        dom: [
-            'dom/base',
-                UA.ieMode < 9 ? 'dom/ie' : ''
-        ]
-    });
-});
+(function () {
+    function init(UA, Feature) {
+        modulex.config('alias', {
+            'modulex-dom': 'dom',
+            'dom/selector': Feature.isQuerySelectorSupported() ? '' : 'query-selector',
+            dom: [
+                'dom/base',
+                    UA.ieMode < 9 ? 'dom/ie' : ''
+            ]
+        });
+    }
+
+    if (typeof UA !== 'undefined') {
+        init(UA, Feature);
+    } else {
+        modulex.use(['modulex-ua', 'modulex-feature'], init);
+    }
+})();
